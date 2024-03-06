12aa0caa847c55199619579d1d0cf8986c9c0eb7
Che <profesor76@hotmail.com>
2024-03-06 14:34:47 -0600

    060324

--- AuthProject/Controllers/HomeController.cs
+++ AuthProject/Controllers/HomeController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Authorization:
+using Microsoft.AspNetCore.Authorization;
 
 namespace AuthProject.Controllers
 {
